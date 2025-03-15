26454b4878cb660d29d6ded247670ff307258a93
Mati-prog <mati.baritono.it@gmail.com>
2025-03-15 16:47:54 +0100

    función para agregar amigos

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Array para agregar los amigos
 let amigos = [];
+// Función para agregar amigos
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let amigo = inputAmigo.value.trim();
+}
